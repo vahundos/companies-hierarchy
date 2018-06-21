fb377ae2bc3f824baf0595bf2574bc2e103ebe75
Vladimir Avdeev <vahundos@gmail.com>
2018-06-21 12:48:37 +0300

    added logging to controllers

--- src/main/java/com/vahundos/companies/web/CompanyAjaxController.java
+++ src/main/java/com/vahundos/companies/web/CompanyAjaxController.java
@@ -2,6 +2,8 @@ package com.vahundos.companies.web;
 
 import com.vahundos.companies.service.CompanyService;
 import com.vahundos.companies.to.CompanyTo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +19,8 @@ public class CompanyAjaxController {
 
     private final CompanyService service;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CompanyAjaxController.class);
+
     @Autowired
     public CompanyAjaxController(CompanyService service) {
         this.service = service;
@@ -29,26 +33,31 @@ public class CompanyAjaxController {
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<CompanyTo> findAll() {
+        LOGGER.debug("find all companies");
         return service.findAll();
     }
 
     @GetMapping(value = "/children", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<CompanyTo> findAllWithChildren() {
+        LOGGER.debug("find all companies with children");
         return service.findAllWithChildren();
     }
 
     @DeleteMapping(value = "/{id}")
     public void delete(@PathVariable int id) {
+        LOGGER.debug("delete company with id = {}", id);
         service.delete(id);
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public void create(@Valid @RequestBody CompanyTo company) {
+        LOGGER.debug("create new company {}", company);
         service.create(company);
     }
 
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public void update(@Valid @RequestBody CompanyTo company) {
+        LOGGER.debug("update company {}", company);
         service.update(company);
     }
 }
--- src/main/java/com/vahundos/companies/web/MainController.java
+++ src/main/java/com/vahundos/companies/web/MainController.java
@@ -1,13 +1,18 @@
 package com.vahundos.companies.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CompanyAjaxController.class);
+
     @RequestMapping("/")
     public String mainPage() {
+        LOGGER.debug("request for root");
         return "main";
     }
 }
